2fbe946f759ddb668b6ddb30d28783c58e510cf3
Keyi Zhang <keyi@stanford.edu>
2021-03-09 22:04:55 -0800

    add item and batch api; need to work on column-based compression

--- src/log.cc
+++ src/log.cc
@@ -1,1 +1,56 @@
 #include "log.hh"
+
+namespace hgdb::log {
+
+LogFile::LogFile(const std::string &filename) : path(filename) {
+    stream = std::ifstream(filename);
+    if (stream.bad()) {
+        throw std::runtime_error("Invalid filename " + filename);
+    }
+}
+
+LogFile::~LogFile() { stream.close(); }
+
+void LogDatabase::add_file(const std::string &filename) {
+    log_files_.emplace_back(std::make_unique<LogFile>(filename));
+}
+
+std::unique_ptr<LogItemBatch> compress(const LogPrintfParser::Format &format,
+                                       std::vector<LogItem> &items,
+                                       std::map<uint64_t, std::vector<uint64_t>> &item_index_,
+                                       uint64_t batch_idx) {
+    // we perform column based storage
+    if (items.empty()) return nullptr;
+    for (auto const &item: items) {
+
+    }
+    return nullptr;
+}
+
+void LogDatabase::parse(const LogPrintfParser &parser) {
+    std::vector<LogItem> batch;
+    batch.reserve(batch_size_);
+    format_ = parser.format();
+
+    for (auto &file_ptr : log_files_) {
+        auto *file = file_ptr.get();
+        // we first parse the file to create raw files
+        std::string line;
+        if (file->stream.bad()) return;
+        // index the positions
+        while (std::getline(file->stream, line)) {
+            if (line.empty()) continue;
+            auto item = parser.parse(line);
+            batch.emplace_back(item);
+
+            if (batch.size() >= batch_size_) {
+                auto p = compress(format_, batch, item_index_, batches_.size());
+                if (p) batches_.emplace_back(std::move(p));
+            }
+        }
+    }
+    auto p = compress(format_, batch, item_index_, batches_.size());
+    if (p) batches_.emplace_back(std::move(p));
+}
+
+}  // namespace hgdb::log
--- src/log.hh
+++ src/log.hh
@@ -1,11 +1,14 @@
 #ifndef HGDB_RTL_LOG_HH
 #define HGDB_RTL_LOG_HH
 
-#include <string>
+#include <fstream>
+#include <map>
 #include <memory>
 #include <set>
+#include <string>
 #include <vector>
-#include <fstream>
+
+namespace hgdb::log {
 
 class LogFile {
 public:
@@ -13,34 +16,75 @@ public:
 
     std::ifstream stream;
     std::string path;
+
+    ~LogFile();
 };
 
 // since logs are semi-structured. we only pick a few attributes that share among different log
 // structures. Notice that log items are directly instantiated from the io locations
 class LogItem {
 public:
-    explicit LogItem(uint64_t time): time(time) {}
+    explicit LogItem(uint64_t time) : time(time) {}
     uint64_t time;
-    uint64_t start_pos = 0;
-    uint64_t end_pos = 0;
 
     // pointing to the log file
     LogFile *file = nullptr;
 
     virtual ~LogItem() = default;
+
+    // we support following types
+    std::vector<int64_t> int_values;
+    std::vector<std::string> str_values;
+    std::vector<double> float_values;
+};
+
+class LogFormatParser {
+public:
+    enum class ValueType { Int, Str, Float };
+    using Format = std::map<std::string, std::pair<ValueType, uint64_t>>;
+    [[nodiscard]] virtual Format format() const = 0;
+    [[nodiscard]] virtual LogItem parse(const std::string &content) const = 0;
+};
+
+// a batch of log items
+class LogItemBatch {
+public:
+    LogItemBatch(uint64_t size, std::vector<char> raw_data, const LogFormatParser::Format &format)
+        : size_(size), raw_data_(std::move(raw_data)), format_(format) {}
+
+    void get_item(uint64_t index, LogItem &item);
+
+private:
+    uint64_t size_;
+    std::vector<char> raw_data_;
+    const LogFormatParser::Format &format_;
+};
+
+class LogPrintfParser : public LogFormatParser {
+public:
+    LogPrintfParser(const std::string &format, const std::vector<std::string> &attr_names);
+
+    LogItem parse(const std::string &content) const override;
+
+    std::map<std::string, std::pair<ValueType, uint64_t>> format() const override;
 };
 
 class LogDatabase {
 public:
     LogDatabase() = default;
+    explicit LogDatabase(uint64_t batch_size) : batch_size_(batch_size) {}
 
     void add_file(const std::string &filename);
+    void parse(const LogPrintfParser &parser);
 
 private:
-    // index item by time
-    // use map so that we can do B-tree operations
-    std::set<uint64_t, std::vector<std::unique_ptr<LogItem>>> items_;
+    uint64_t batch_size_ = 1024;
+    std::vector<std::unique_ptr<LogItemBatch>> batches_;
+    std::map<uint64_t, std::vector<uint64_t>> item_index_;
     std::vector<std::unique_ptr<LogFile>> log_files_;
+    LogFormatParser::Format format_;
 };
+
+}  // namespace hgdb::log
 
 #endif  // HGDB_RTL_LOG_HH
